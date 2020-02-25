fefcf82e67eb4c6c319ebb8b74c30373bebbdb63
blody09 <blakeody9@gmail.com>
2020-02-25 08:43:38 -0500

    First upload of coffee shop not finished

---
+++ CoffeeShopLab18/Controllers/RegistrationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShopLab18.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopLab18.Controllers
+{
+    public class RegistrationController : Controller
+    {
+       
+        public IActionResult RegistrationForm()
+        {
+            return View(new User());
+        }
+
+        public IActionResult Summary(User user)
+        {
+            if(ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                return View("RegistrationForm", user);
+            }
+           
+        }
+    }
+}
---
+++ CoffeeShopLab18/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopLab18.Models
+{
+    public class User
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 30 characters.")]
+        public string UserName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9{5,30}]+@[a-zA-A0-9{5,10}]+\.[a-zA-Z0-9{2,3}]+$", ErrorMessage = "Please enter a valid email address.")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*\d).{8,}$", ErrorMessage = "Must be between 1-30 Characters.")]
+        
+        public string Password { get; set; }
+        
+        public void GetUserName()
+        {
+            UserName = UserName;
+        }
+        
+    }
+
+}
